db172a52a9dfd0bed4d27d6ad85dd91e0eb6c476
PeterfiLaci <113706063+placi0325@users.noreply.github.com>
2023-05-30 13:45:06 +0200

    create spring app

---
+++ src/main/java/com/codecool/elproyectegrande/ElProyecteGrandeSprint1JavaPlaci0325Application.java
@@ -0,0 +1,13 @@
+package com.codecool.elproyectegrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElProyecteGrandeSprint1JavaPlaci0325Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElProyecteGrandeSprint1JavaPlaci0325Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/elproyectegrande/ElProyecteGrandeSprint1JavaPlaci0325ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.elproyectegrande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElProyecteGrandeSprint1JavaPlaci0325ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
